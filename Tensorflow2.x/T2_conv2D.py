2abe6c0e2e1d9811684754daee1bef45cec799e9
Sang-Hoon Park <timesea821@naver.com>
2020-07-12 22:38:34 +0900

    modified script

--- Tensorflow2.x/T2_conv2D.py
+++ Tensorflow2.x/T2_conv2D.py
@@ -23,11 +23,9 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=(28, 28, 1)),
   tf.keras.layers.MaxPool2D(2, 2),
   tf.keras.layers.Dropout(0.3),
-  tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
-  tf.keras.layers.MaxPool2D(2, 2),
-  tf.keras.layers.Dropout(0.3),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.3),
   tf.keras.layers.Dense(10, activation='softmax')
 ])
 model.summary()
@@ -36,5 +34,5 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+model.fit(x_train, y_train, epochs=50, callbacks=[callbacks])
 model.evaluate(x_test, y_test, verbose=1)
