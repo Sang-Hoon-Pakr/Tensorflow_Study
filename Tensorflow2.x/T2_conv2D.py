f03405de295f1fc878268695351c2efcde414fa7
Sang-Hoon Park <timesea821@naver.com>
2020-07-13 11:32:12 +0900

    new

--- Tensorflow2.x/T2_conv2D.py
+++ Tensorflow2.x/T2_conv2D.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 #Callback Example 
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
+    print("epoch", epoch, "acc", logs.get('accuracy'))
     if(logs.get('accuracy')>0.99):
       print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
