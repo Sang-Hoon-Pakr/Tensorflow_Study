63df4fc0ca664bfa514a23e584e5d1c0a4e271e9
Sang-Hoon Park <timesea821@naver.com>
2020-07-16 00:21:48 +0900

    new

--- Tensorflow2.x/T2_Course3_W1L1.py
+++ Tensorflow2.x/T2_Course3_W1L1.py
@@ -16,7 +16,8 @@ sequences = tokenizer.texts_to_sequences(sentences)#시퀀스 텍스트
 padded = pad_sequences(sequences) #패딩 처리 
 padded_post = pad_sequences(sequences, padding='post') #패딩 처리 
 padded_post5 = pad_sequences(sequences, padding='post', maxlen=5) #패딩 처리 <최고 긴것이 ..5 이상이면 앞에서부터 짤림>
-padded_postpost5 = pad_sequences(sequences, padding='post',truncating='post', maxlen=5) #패딩 처리 <최고 긴것이 ..5 이상이면 앞에서부터 짤림>
+padded_postpost5 = pad_sequences(sequences, padding='post',truncating='post', maxlen=5) #패딩 처리 <최고 긴것이 ..5 이상이면 뒤에껄 자름>
+
 print("word_index: ", word_index)
 print("sequences: ", sequences)
 print("padded: ", padded)
@@ -30,4 +31,6 @@ test_data = [
 ]
 
 test_seq = tokenizer.texts_to_sequences(test_data)
+padded_test_data = pad_sequences(test_seq, maxlen=10)
 print("test_deq: ",test_seq)
+print("padded test data: ",padded_test_data)
