91eb071ef6109e1f6665e938d00a217651e4dc48
Sang-Hoon Park <timesea821@naver.com>
2020-09-03 21:51:31 +0900

    new

--- Tensorflow2.x/T2_mnist_csv.py
+++ Tensorflow2.x/T2_mnist_csv.py
@@ -97,7 +97,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(26, activation='softmax')
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 # Compile Model. 
